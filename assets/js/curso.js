60ed55037e339ff7a62c879ebd61f8fe418c4fb1
juan <juan@anita.local>
2014-04-01 13:18:37 -0600

    curso5j

---
+++ assets/js/curso.js
@@ -0,0 +1,28 @@
+//var mostrarmensaje= false;
+//var mostrar =true
+
+//if(!mostrarmensaje && mostrar) {
+//	alert("entra")
+//}
+
+var numero1 =5:
+var numero2 =8:
+
+if (numero1>numero2) {
+
+ 	alert("numero1 no es mayor a numero2")
+}
+
+if () {
+	alert(numero2 es positivo)
+}
+
+if () {
+	alert(numero1 es negativo o distinto a cero)
+}
+
+if () {
+	alert(incrementar en 1 unidad el valor de numero1  a lo hace mayor o igual  al numero2)
+}
+
+
