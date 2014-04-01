5feeefd863bc6a19ae17ee7dee23da669b56a8e0
Luis Navarrete <luisnn@iMac-de-IECA-GAUSS-JORDAN.local>
2014-04-01 13:43:36 -0600

    curso_3_L

--- assets/js/curso.js
+++ assets/js/curso.js
@@ -1,3 +1,32 @@
+<<<<<<< HEAD
+//var mostrarmensaje= false;
+//var mostrar =true
+
+//if(!mostrarmensaje && mostrar) {
+//	alert("entra")
+//}
+
+var numero1 =5:
+var numero2 =8:
+
+if (numero1>numero2) {
+
+ 	alert("numero1 no es mayor a numero2")
+}
+
+if () {
+	alert(numero2 es positivo)
+}
+
+if () {
+	alert(numero1 es negativo o distinto a cero)
+}
+
+if () {
+	alert(incrementar en 1 unidad el valor de numero1  a lo hace mayor o igual  al numero2)
+}
+
+=======
 //var variable1="hola", variable2="hola", variable3="adiosadlalldldd"
 //	alert(variable2>=variable3)
 
@@ -6,6 +35,7 @@ var mostrado=true
 if (!mostrarMensaje && mostrado) { || o
 	alert('Entro')
 }*/
+>>>>>>> d852d35a47f35b0dc354f6af099b8431b4e21c81
 
 /*var nombre_array=[valor1,valor2,valor3]
 allert(nombre_array[0])*/
---
+++ assets/js/curso_5j.js
@@ -0,0 +1,19 @@
+var numero1 =5;
+var numero2 =8;
+
+if (numero1<=numero2) {
+	alert("numero1 no es mayor a numero2")
+}
+
+if (numero2>=0) {
+	alert("numero2 es positivo")
+}
+
+if (numero1<0 || numero1!=0 ) {
+	alert("numero1 es negativo o distinto a cero")
+}
+
+if (numero +1 <= numero2) {
+	alert("incrementar en 1 unidad el valor de numero1  a lo hace mayor o igual  al numero2")
+}
+
