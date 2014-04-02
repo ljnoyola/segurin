10cde92daad83193d21b20ff52d20f9c7548e901
Luis Navarrete <luisnn@iMac-de-IECA-GAUSS-JORDAN.local>
2014-04-02 13:31:33 -0600

    eje 6 DNI

---
+++ assets/js/if.js
@@ -0,0 +1,33 @@
+/*var edad=18;
+
+if(edad>=18){
+	alert("Eres mayor de edad");
+}
+else{
+	alert("Eres menor de edad");
+}*/
+
+
+/*var nombre="luis";
+
+if(nombre==""){
+	alert("Aun no has escrito nu nombre");
+}
+else{
+	alert("hemos guardado tu nombre");
+}*/
+
+/*var edad=39;
+
+if(edad<12){
+	alert("Eres muy pequeño");
+}
+else if(edad<19){
+	alert("Eres un adolescente");
+}
+else if(edad<35){
+	alert("Aun sigues siendo joven");
+}
+else {
+	alert("Piensa en cuidarte un poco mas")
+}*/
