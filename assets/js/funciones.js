a782a7f4db851247c224f1d47a3b843e522cb15a
Luis Navarrete <luisnn@iMac-de-IECA-GAUSS-JORDAN.local>
2014-04-03 13:44:44 -0600

    funciones javascrip

---
+++ assets/js/funciones.js
@@ -0,0 +1,39 @@
+/*//numero de elementos
+var numeroletras=mensaje.length;
+
+//concatenar
+var mensaje1="hola";
+var mensaje2="mundo";
+var mensaje=mensaje1+mensaje2// mesaje="hola mundo"
+
+var mensaje1="hola";
+var mensaje2=3;
+var mensaje=mensaje1+mensaje2;// mesaje="hola 3"
+
+var mensaje1="hola";
+var mensaje2=mensaje1.toUpperCase();//mensaje2=HOLA
+
+var mensaje1="Hola";
+var mensaje2=mensaje1.toLowerCase();//mensaje2=hola
+
+var mensaje="Hola mundo";
+var porcion=mensaje.substring(1,8);//porcion="ola mun"
+var porcion=mensaje.substring(3,4);//porcion="a"*/
+
+/*var mensaje="Hola Mundo, soy una cadena de texto!";
+var palabras=mensaje.split(" ");
+
+for(i in palabras){
+	alert(palabras[i]);
+}*/
+
+/*var numero=4564.34567;
+numero1.toFixed(2);//4564.35
+numero1.toFixed(6);//4564,345670
+numero1.toFixed();//4564*/
+
+/*function nombre_funcion(){
+	....
+
+}*/
+
